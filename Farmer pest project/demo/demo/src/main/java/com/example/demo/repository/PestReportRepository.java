3a37637f0d93abe5bb2ba9e42157abb99844d615
Sinchana Suresh Ganiga <sinci2573@gmail.com>
2025-09-02 21:30:02 +0530

    Delete Farmer pest project/demo/demo directory

--- Farmer pest project/demo/demo/src/main/java/com/example/demo/repository/PestReportRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.PestReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//Repository to store farmer-submitted reports for analytics
-public interface PestReportRepository extends JpaRepository<PestReport, Long> {
-}
